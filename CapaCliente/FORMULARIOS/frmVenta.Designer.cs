a3579607863d22ebb8ba0433b2fa06c3879f0e66
PRO-RAM <ornelasvaldez07@gmail.com>
2025-11-21 07:24:56 -0600

    Clase Vantas y Mod frmVentas

--- CapaCliente/FORMULARIOS/frmVenta.Designer.cs
+++ CapaCliente/FORMULARIOS/frmVenta.Designer.cs
@@ -31,24 +31,29 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmVenta));
             this.panel5 = new System.Windows.Forms.Panel();
             this.TXTFILTRO = new System.Windows.Forms.TextBox();
-            this.DgProductos = new System.Windows.Forms.DataGridView();
+            this.button9 = new System.Windows.Forms.Button();
             this.panel4 = new System.Windows.Forms.Panel();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.button9 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.button6 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel5.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.DgProductos)).BeginInit();
             this.panel4.SuspendLayout();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel5
@@ -70,20 +75,28 @@
             this.TXTFILTRO.Multiline = true;
             this.TXTFILTRO.Name = "TXTFILTRO";
             this.TXTFILTRO.Size = new System.Drawing.Size(868, 49);
-            this.TXTFILTRO.TabIndex = 23;
+            this.TXTFILTRO.TabIndex = 1;
             // 
-            // DgProductos
+            // button9
             // 
-            this.DgProductos.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(242)))), ((int)(((byte)(248)))));
-            this.DgProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DgProductos.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.DgProductos.Location = new System.Drawing.Point(0, 60);
-            this.DgProductos.Name = "DgProductos";
-            this.DgProductos.RowHeadersWidth = 51;
-            this.DgProductos.RowTemplate.Height = 24;
-            this.DgProductos.Size = new System.Drawing.Size(930, 656);
-            this.DgProductos.TabIndex = 30;
-            this.DgProductos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgProductos_CellContentClick);
+            this.button9.BackColor = System.Drawing.Color.Transparent;
+            this.button9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button9.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button9.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button9.FlatAppearance.BorderSize = 0;
+            this.button9.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.button9.FlatAppearance.MouseOverBackColor = System.Drawing.Color.MediumTurquoise;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.Font = new System.Drawing.Font("Clarendon BT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(57)))), ((int)(((byte)(43)))));
+            this.button9.Image = ((System.Drawing.Image)(resources.GetObject("button9.Image")));
+            this.button9.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button9.Location = new System.Drawing.Point(868, 0);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(62, 49);
+            this.button9.TabIndex = 24;
+            this.button9.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button9.UseVisualStyleBackColor = false;
             // 
             // panel4
             // 
@@ -123,6 +136,7 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(246)))), ((int)(((byte)(244)))));
+            this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.button5);
             this.panel1.Controls.Add(this.button4);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
@@ -130,47 +144,6 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(930, 150);
             this.panel1.TabIndex = 28;
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(246)))), ((int)(((byte)(244)))));
-            this.panel2.Controls.Add(this.button6);
-            this.panel2.Controls.Add(this.pictureBox1);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel2.Location = new System.Drawing.Point(930, 60);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(200, 851);
-            this.panel2.TabIndex = 27;
-            // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel3.Location = new System.Drawing.Point(0, 0);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1130, 60);
-            this.panel3.TabIndex = 26;
-            // 
-            // button9
-            // 
-            this.button9.BackColor = System.Drawing.Color.Transparent;
-            this.button9.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button9.Dock = System.Windows.Forms.DockStyle.Right;
-            this.button9.FlatAppearance.BorderColor = System.Drawing.Color.White;
-            this.button9.FlatAppearance.BorderSize = 0;
-            this.button9.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
-            this.button9.FlatAppearance.MouseOverBackColor = System.Drawing.Color.MediumTurquoise;
-            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button9.Font = new System.Drawing.Font("Clarendon BT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(57)))), ((int)(((byte)(43)))));
-            this.button9.Image = ((System.Drawing.Image)(resources.GetObject("button9.Image")));
-            this.button9.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.button9.Location = new System.Drawing.Point(868, 0);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(62, 49);
-            this.button9.TabIndex = 24;
-            this.button9.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.button9.UseVisualStyleBackColor = false;
             // 
             // button5
             // 
@@ -189,7 +162,7 @@
             this.button5.Location = new System.Drawing.Point(151, 0);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(163, 150);
-            this.button5.TabIndex = 8;
+            this.button5.TabIndex = 4;
             this.button5.Text = "ELIMINAR";
             this.button5.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.button5.UseVisualStyleBackColor = false;
@@ -211,10 +184,21 @@
             this.button4.Location = new System.Drawing.Point(0, 0);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(151, 150);
-            this.button4.TabIndex = 7;
+            this.button4.TabIndex = 3;
             this.button4.Text = "REPETIR";
             this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.button4.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(246)))), ((int)(((byte)(244)))));
+            this.panel2.Controls.Add(this.button6);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(930, 60);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 851);
+            this.panel2.TabIndex = 27;
             // 
             // button6
             // 
@@ -233,7 +217,7 @@
             this.button6.Location = new System.Drawing.Point(0, 701);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(200, 150);
-            this.button6.TabIndex = 6;
+            this.button6.TabIndex = 5;
             this.button6.Text = "COBRAR";
             this.button6.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.button6.UseVisualStyleBackColor = false;
@@ -250,13 +234,79 @@
             this.pictureBox1.TabIndex = 16;
             this.pictureBox1.TabStop = false;
             // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1130, 60);
+            this.panel3.TabIndex = 26;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(9, 179);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(907, 531);
+            this.dataGridView1.TabIndex = 32;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(759, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 46);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "AGREGAR";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Clarendon BT", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(3, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(179, 41);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "CODIGO:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(180, 127);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(236, 31);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Clarendon BT", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(12, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 27);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "EMPLEADO:";
+            // 
             // frmVenta
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1130, 911);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.panel5);
-            this.Controls.Add(this.DgProductos);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel2);
@@ -265,13 +315,16 @@
             this.Text = "frmVenta";
             this.panel5.ResumeLayout(false);
             this.panel5.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.DgProductos)).EndInit();
             this.panel4.ResumeLayout(false);
             this.panel4.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -280,7 +333,6 @@
         private System.Windows.Forms.Panel panel5;
         private System.Windows.Forms.TextBox TXTFILTRO;
         private System.Windows.Forms.Button button9;
-        public System.Windows.Forms.DataGridView DgProductos;
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
@@ -291,5 +343,10 @@
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
     }
 }
---
+++ CapaNegocio/CLASES/Ventas.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaNegocio.CLASES
+{
+    public class Ventas
+    {
+
+        static Conexion co = new Conexion();
+        SqlConnection con = new SqlConnection(co.conexion);
+        SqlCommand comando = new SqlCommand();
+
+        // ====== CAMPOS MAESTRO ======
+        public int idVenta, idCliente, idEmpleados, idFormaPago;
+        public string Folio;
+        public DateTime Fecha;
+        public decimal Total;
+
+        // ====== LISTA DE DETALLES ======
+        public List<VentaDetalle> Detalles { get; set; } = new List<VentaDetalle>();
+
+        public Ventas()
+        {
+            comando.Connection = con;
+        }
+        public Ventas(string sConexion)
+        {
+            con.ConnectionString = sConexion; //Asegura la comparacion de valores
+            comando.Connection = con;
+        }
+
+        public class VentaDetalle
+        {
+            public int idVentaDetalle { get; set; }
+            public int idVenta { get; set; }
+            public int idProducto { get; set; }
+            public int CantidadProducto { get; set; }
+            public decimal PrecioProducto { get; set; }
+            public decimal SubTotal { get; set; }
+        }
+
+        public string Guardar(Ventas venta)
+        {
+            string mensaje = "";
+
+            using (SqlConnection con = new SqlConnection(co.conexion))
+            using (SqlCommand cmd = new SqlCommand("SP_Venta", con))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                // Campos Para el Maestro
+                cmd.Parameters.AddWithValue("@op", 1);
+                cmd.Parameters.AddWithValue("@idVenta",
+                    venta.idVenta == 0 ? (object)DBNull.Value : venta.idVenta);
+                cmd.Parameters.AddWithValue("@idCliente", venta.idCliente);
+                cmd.Parameters.AddWithValue("@idEmpleados",
+                    venta.idEmpleados == 0 ? (object)DBNull.Value : venta.idEmpleados);
+                cmd.Parameters.AddWithValue("@idFormaPago", venta.idFormaPago);
+                cmd.Parameters.AddWithValue("@Folio", venta.Folio);
+                cmd.Parameters.AddWithValue("@Fecha", venta.Fecha);
+
+                // Campos Para el Tipo Tabla 
+                DataTable dtDetalles = new DataTable();
+                dtDetalles.Columns.Add("idVentaDetalle", typeof(int));
+                dtDetalles.Columns.Add("idVenta", typeof(int));
+                dtDetalles.Columns.Add("idProducto", typeof(int));
+                dtDetalles.Columns.Add("CantidadProducto", typeof(int));
+                dtDetalles.Columns.Add("PrecioProducto", typeof(decimal));
+                dtDetalles.Columns.Add("subTotal", typeof(decimal));
+
+                // Se llena el Detalle
+                foreach (var d in venta.Detalles)
+                {
+                    dtDetalles.Rows.Add(
+                        0,
+                        0,
+                        d.idProducto,
+                        d.CantidadProducto,
+                        d.PrecioProducto,
+                        d.SubTotal
+                    );
+                }
+
+                // Enviar el Tipo Tabla al Procedimiento Almacenado
+                SqlParameter tvp = cmd.Parameters.AddWithValue("@Detalles", dtDetalles);
+                tvp.SqlDbType = SqlDbType.Structured;
+                tvp.TypeName = "DetalleVenta";
+
+                con.Open();
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        mensaje = reader["Mensaje"].ToString();
+                        venta.idVenta = Convert.ToInt32(reader["idVenta"]);
+                    }
+                    else
+                    {
+                        mensaje = "Venta guardada correctamente";
+                    }
+                }
+
+                con.Close();
+            }
+
+            return mensaje;
+        }
+
+        public string Eliminar()
+        {
+            string mensaje = "";
+            comando.CommandType = CommandType.StoredProcedure; //Confirma que los datos se obtendran de un PA
+            comando.CommandText = "SP_Venta"; //Nombre del PA
+            comando.Parameters.Clear(); //Limpia los parametros, por si queda alguno en cache
+            comando.Parameters.AddWithValue("@op", 3);
+            comando.Parameters.AddWithValue("@idVenta", idVenta);
+            con.Open(); //Abre conexion
+            comando.ExecuteNonQuery(); //Ejecuta cuando no es una consulta
+            mensaje = "CAMPO ELIMINADO";
+
+            con.Close();
+            return mensaje;
+        }
+    }
+}
